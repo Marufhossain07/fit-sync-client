56c9725819d50d0c7a20ffc4d0201b0ce09d5eee
Marufhossain07 <marufprogrammer07@gmail.com>
2024-06-01 22:31:53 +0600

    navbar and error page added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,47 @@
-import {Button, Navbar as FlowbiteNavbar} from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Avatar, Button, Dropdown, Navbar as FlowbiteNavbar } from "flowbite-react";
+import { Link, NavLink } from "react-router-dom";
 
 const FlowbiteNavbarbar = () => {
+    const user = false
     const links = <>
-    <Link to='/'><FlowbiteNavbar.Link>Home</FlowbiteNavbar.Link></Link>
-    <Link to='/all-trainer'><FlowbiteNavbar.Link>All Trainer </FlowbiteNavbar.Link></Link>
-    <Link to='/all-classes'><FlowbiteNavbar.Link>All Classes</FlowbiteNavbar.Link></Link>
-    <Link to='/dashboard'><FlowbiteNavbar.Link> Dashboard</FlowbiteNavbar.Link></Link>
-    <Link to='/community'><FlowbiteNavbar.Link> Community</FlowbiteNavbar.Link></Link>
+        <NavLink to='/'><FlowbiteNavbar.Link className="text-base font-inter">Home</FlowbiteNavbar.Link></NavLink>
+        <NavLink to='/all-trainer'><FlowbiteNavbar.Link className="text-base font-inter">All Trainer </FlowbiteNavbar.Link></NavLink>
+        <NavLink to='/all-classes'><FlowbiteNavbar.Link className="text-base font-inter" >All Classes</FlowbiteNavbar.Link></NavLink>
+        {
+            user && <NavLink to='/dashboard'><FlowbiteNavbar.Link className="text-base font-inter"> Dashboard</FlowbiteNavbar.Link></NavLink>
+        }
+        <NavLink to='/community'><FlowbiteNavbar.Link className="text-base font-inter"> Community</FlowbiteNavbar.Link></NavLink>
     </>
     return (
-        <div>
-            <FlowbiteNavbar fluid rounded>
-                <FlowbiteNavbar.Brand href="https://flowbite-react.com">
-                    <h3 className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">FitSync</h3>
+        <div className="container mx-auto mt-5">
+            <FlowbiteNavbar fluid rounded className="p-0">
+                <FlowbiteNavbar.Toggle />
+                <FlowbiteNavbar.Brand>
+                    <h3 className="self-center whitespace-nowrap text-3xl md:text-3xl lg:text-4xl text-[#003049] font-semibold font-sedan dark:text-white">Fit<span className="text-[#d62828]">Sync</span></h3>
                 </FlowbiteNavbar.Brand>
                 <div className="flex md:order-2">
-                    <Link to='/login'><Button>Login</Button></Link>
-                    <FlowbiteNavbar.Toggle />
+                    {
+                        user? <Dropdown
+                        arrowIcon={false}
+                        inline
+                        label={
+                            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+                        }
+                    >
+                        <Dropdown.Header>
+                            <span className="block text-sm">Bonnie Green</span>
+                            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+                        </Dropdown.Header>
+                        <Dropdown.Item>Log Out</Dropdown.Item>
+                    </Dropdown> : <Link to='/login'><Button>Login</Button></Link>
+                    }
+                    
+                    
                 </div>
-                <FlowbiteNavbar.Collapse>
-        {links}
+                <FlowbiteNavbar.Collapse id="nav">
+                    {links}
                 </FlowbiteNavbar.Collapse>
+                
             </FlowbiteNavbar>
         </div>
     );
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/home/Home";
+import Error from "../components/error/Error";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Main></Main>,
+      errorElement: <Error></Error>,
       children: [
         {
             path: '/',
